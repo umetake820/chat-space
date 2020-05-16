2238c3c16d06dce6b774e360c879444fc04d7dc8
umetake820 <umetac82@gmail.com>
2020-05-17 01:14:04 +0900

    initial commit

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,7 +2,7 @@ $(function(){
 
   function buildHTML(message){
     if (message.image) {
-      var html = `<div class="contents__messages">
+      var html = `<div class="contents__messages" data-message-id=${message.id}>
                     <div class="contents__messages__data">
                       ${message.user_name}
                       <span>
@@ -15,7 +15,7 @@ $(function(){
                     </div>
                   </div>`
     } else {
-      var html = `<div class="contents__messages">
+      var html = `<div class="contents__messages" data-message-id=${message.id}>
                     <div class="contents__messages__data">
                       ${message.user_name}
                       <span>
@@ -55,4 +55,30 @@ $(function(){
     $('input').prop('disabled', false);
   });
   });
+
+  var reloadMessages = function() {
+    var last_message_id = $('.contents__messages:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+      var insertHTML = '';
+      $.each(messages, function(i, message) {
+        insertHTML += buildHTML(message)
+      });
+      $('.contents').append(insertHTML);
+      $('.contents').animate({ scrollTop: $('.contents')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
